378afeb0c1eb2616c9b4c7340cfbc5d3d1c1dd82
Robbert Rijkse <42551307+sestary@users.noreply.github.com>
2022-12-28 20:45:12 -0500

    add print

--- dns/bind/src/opnsense/mvc/app/controllers/OPNsense/Bind/Api/AclController.php
+++ dns/bind/src/opnsense/mvc/app/controllers/OPNsense/Bind/Api/AclController.php
@@ -42,6 +42,7 @@ class AclController extends ApiMutableModelControllerBase
         # Loops through all the Acls and ensure the name doesn't exist
         foreach ($this->searchBase('acls.acl', array("name"))["rows"] as $existingAcl) {
             if ($existingAcl["name"] == $name && $existingAcl["uuid"] != $excludeUUID) {
+                print_r($existingAcl)
                 return true;
             }
         }
